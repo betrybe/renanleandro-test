1f316a5789c6a405f00d3c6739623098bace81b5
renanlf <leandrorenanf@gmail.com>
2021-11-01 21:03:53 -0300

    Implementada soma dos itens do carrinho.

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+/* Consultas realizadas */
+
+/**
+ * função responsável por carregar os produtos da API
+ * retornando uma Promise com os produtos no formato esperado.
+ */
+ async function getProducts(){
+  return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((resp) => resp.json())
+  .then((json) => json.results)
+  .then((results) => results.map(result => 
+      ({
+        sku : result.id,
+        name : result.title,
+        image : result.thumbnail
+      })
+    )
+  );
+}
+
+/**
+ * função responsável por carregar os produtos do carrinho da API
+ * e retornar uma Promise com os produtos no formato esperado.
+ * @param {int} sku 
+ */
+async function getProductItem(sku){
+  return fetch(`https://api.mercadolibre.com/items/${sku}`)
+  .then((resp) => resp.json())
+  .then((result) => 
+    ({
+      sku : result.id,
+      name : result.title,
+      salePrice : result.price
+    })
+  );
+}
+
+/* FIM - Consultas */
+
+function updateTotalPrice(amount){
+  const cartPriceElement = document.getElementById('cart_price');
+
+  let price = parseFloat(cartPriceElement.innerText);
+  price = price ? price : 0; 
+  price += amount;
+
+  cartPriceElement.innerText = price;
+}
+
+/**
+ * função responsável por remover o produto na posição index do repo local (local storage)
+ * @param {int} index 
+ */
+function removeFromLocalStorage(index){
+  if(localStorage.skus){
+    const skus = JSON.parse(localStorage.skus);
+    skus.splice(index,1)
+    localStorage.skus = JSON.stringify(skus);
+  }
+}
+
+function cartItemClickListener(event) {
+  const cartItemsElement = event.target.parentNode;
+  index = Array.from(cartItemsElement.childNodes).indexOf(event.target);
+  event.target.remove();
+  removeFromLocalStorage(index);
+
+  const itemPriceElements = event.target.getElementsByClassName('item_price');
+
+  if(itemPriceElements.length > 0){
+    const price = parseFloat(itemPriceElements[0].innerText);
+    updateTotalPrice(-price);
+  }
+}
+
+function createPriceItemElement(salePrice){
+  const span = document.createElement('span');
+  span.innerText = salePrice;
+  span.className = 'item_price';
+  return span;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $`;
+  li.addEventListener('click', cartItemClickListener);
+  // modificado para manipular o preço de forma mais simples.
+  li.appendChild(createPriceItemElement(salePrice));
+  return li;
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -16,6 +108,36 @@ function createClickableElement(element, className, innerText, clickListener){
   const e = createCustomElement(element, className, innerText);
   e.addEventListener('click', clickListener);
   return e;
+}
+
+/**
+ * função responsável por salvar o sku localmente (local storage)
+ * 
+ * OBS.: optei por salvar apenas o sku ao invés de todas as infos do
+ * produto dado que, em situações reais, o preço do produto pode variar
+ * após a adição no carrinho.
+ * @param {int} sku 
+ */
+ function addToLocalStorage(sku){
+  const skus = localStorage.skus ? JSON.parse(localStorage.skus) : []
+  skus.push(sku);
+  localStorage.skus = JSON.stringify(skus);
+}
+
+/**
+ * função responsável por inserir os produtos do carrinho da API
+ * na sua respectiva li e atualizando o preço total.
+ * @param {int} sku 
+ */
+ function addToCartItems(sku){
+  const olCartItems = document.getElementById('cart_items');
+  
+  getProductItem(sku)
+  .then((product) => {
+    const cartItemElement = createCartItemElement(product);
+    olCartItems.appendChild(cartItemElement);
+    updateTotalPrice(product.salePrice);
+  });
 }
 
 function addToCartClickListener(event){
@@ -40,107 +162,30 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  const cartItemsElement = event.target.parentNode;
-  index = Array.from(cartItemsElement.childNodes).indexOf(event.target);
-  event.target.remove();
-  removeFromLocalStorage(index);
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-/* Consultas realizadas */
-
 /**
- * função responsável por carregar os produtos da API
- * e inseri-los na section items.
+ * função responsável por adicionar os produtos na seção items
  */
  function loadProducts(){
-  const sectionItems = document.getElementById('items');
-
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((resp) => resp.json())
-    .then((json) => json.results)
-    .then((results) => results.forEach(result => {
-        const sectionProduct = createProductItemElement({
-          sku : result.id,
-          name : result.title,
-          image : result.thumbnail
-        });
-        sectionItems.appendChild(sectionProduct);
-        sectionItems.appendChild(sectionProduct);
-      })
-    );
-}
-
-/**
- * função responsável por carregar os produtos do carrinho da API
- * e inseri-los na sua respectiva li.
- * @param {int} sku 
- */
-function addToCartItems(sku){
-  const olCartItems = document.getElementById('cart_items');
-  fetch(`https://api.mercadolibre.com/items/${sku}`)
-    .then((resp) => resp.json())
-    .then((result) => {
-      const cartItemElement = createCartItemElement({
-        sku : result.id,
-        name : result.title,
-        salePrice : result.price
-      });
-      olCartItems.appendChild(cartItemElement);
-    });
-}
-
-/* FIM - Consultas */
-
-/* Local Storage */
-
-/**
- * função responsável por salvar o sku localmente (local storage)
- * 
- * OBS.: optei por salvar apenas o sku ao invés de todas as infos do
- * produto dado que, em situações reais, o preço do produto pode variar
- * após a adição no carrinho.
- * @param {int} sku 
- */
-function addToLocalStorage(sku){
-  const skus = localStorage.skus ? JSON.parse(localStorage.skus) : []
-  skus.push(sku);
-  localStorage.skus = JSON.stringify(skus);
-}
-
-/**
- * função responsável por remover o produto na posição index do repo local (local storage)
- * @param {int} index 
- */
-function removeFromLocalStorage(index){
-  if(localStorage.skus){
-    const skus = JSON.parse(localStorage.skus);
-    skus.splice(index,1)
-    localStorage.skus = JSON.stringify(skus);
-  }
+  const itemsSection = document.getElementById('items');
+  getProducts()
+  .then( products => products.map(createProductItemElement))
+  .then( productItemElements => productItemElements
+    .forEach(productItemElement => itemsSection.appendChild(productItemElement)) 
+  )
 }
 
 /**
  * função responsável por carregar os produtos do carrinho do repo local (local storage)
  */
-function loadFromLocalStorage(){
+ function loadFromLocalStorage(){
   if(localStorage.skus){
     const skus = JSON.parse(localStorage.skus);
     skus.forEach(addToCartItems);
   }
 }
 
-/* FIM - Local Storage */
-
 window.onload = () => {
+  updateTotalPrice(0);
   loadProducts();
   loadFromLocalStorage();
 };
