487790ec7166ed72fdc0ff53340e3e0cbcdc3b1c
renanlf <leandrorenanf@gmail.com>
2021-11-03 16:58:30 -0300

    Correções do ESLint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createLoadingDiv(){
+function createLoadingDiv() {
   const div = document.createElement('div');
   div.className = 'loading';
   div.id = 'loading';
@@ -11,18 +11,16 @@ function createLoadingDiv(){
  * função responsável por carregar os produtos da API
  * retornando uma Promise com os produtos no formato esperado.
  */
- async function getProducts(){
+ async function getProducts() {
   return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((resp) => resp.json())
   .then((json) => json.results)
-  .then((results) => results.map(result => 
-      ({
-        sku : result.id,
-        name : result.title,
-        image : result.thumbnail
-      })
-    )
-  );
+  .then((results) => results.map((result) => 
+    ({
+      sku: result.id,
+      name: result.title,
+      image: result.thumbnail,
+    })));
 }
 
 /**
@@ -30,25 +28,24 @@ function createLoadingDiv(){
  * e retornar uma Promise com os produtos no formato esperado.
  * @param {int} sku 
  */
-async function getProductItem(sku){
+async function getProductItem(sku) {
   return fetch(`https://api.mercadolibre.com/items/${sku}`)
   .then((resp) => resp.json())
   .then((result) => 
     ({
-      sku : result.id,
-      name : result.title,
-      salePrice : result.price
-    })
-  );
+      sku: result.id,
+      name: result.title,
+      salePrice: result.price,
+    }));
 }
 
 /* FIM - Consultas */
 
-function updateTotalPrice(amount){
+function updateTotalPrice(amount) {
   const cartPriceElement = document.getElementById('cart_price');
 
   let price = parseFloat(cartPriceElement.innerText);
-  price = price ? price : 0; 
+  price = price || 0; 
   price += amount;
 
   cartPriceElement.innerText = price;
@@ -58,29 +55,29 @@ function updateTotalPrice(amount){
  * função responsável por remover o produto na posição index do repo local (local storage)
  * @param {int} index 
  */
-function removeFromLocalStorage(index){
-  if(localStorage.skus){
+function removeFromLocalStorage(index) {
+  if (localStorage.skus) {
     const skus = JSON.parse(localStorage.skus);
-    skus.splice(index,1)
+    skus.splice(index, 1);
     localStorage.skus = JSON.stringify(skus);
   }
 }
 
 function cartItemClickListener(event) {
   const cartItemsElement = event.target.parentNode;
-  index = Array.from(cartItemsElement.childNodes).indexOf(event.target);
+  const index = Array.from(cartItemsElement.childNodes).indexOf(event.target);
   event.target.remove();
   removeFromLocalStorage(index);
 
   const itemPriceElements = event.target.getElementsByClassName('item_price');
 
-  if(itemPriceElements.length > 0){
+  if (itemPriceElements.length > 0) {
     const price = parseFloat(itemPriceElements[0].innerText);
     updateTotalPrice(-price);
   }
 }
 
-function createPriceItemElement(salePrice){
+function createPriceItemElement(salePrice) {
   const span = document.createElement('span');
   span.innerText = salePrice;
   span.className = 'item_price';
@@ -111,7 +108,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createClickableElement(element, className, innerText, clickListener){
+function createClickableElement(element, className, innerText, clickListener) {
   const e = createCustomElement(element, className, innerText);
   e.addEventListener('click', clickListener);
   return e;
@@ -125,8 +122,8 @@ function createClickableElement(element, className, innerText, clickListener){
  * após a adição no carrinho.
  * @param {int} sku 
  */
- function addToLocalStorage(sku){
-  const skus = localStorage.skus ? JSON.parse(localStorage.skus) : []
+ function addToLocalStorage(sku) {
+  const skus = localStorage.skus ? JSON.parse(localStorage.skus) : [];
   skus.push(sku);
   localStorage.skus = JSON.stringify(skus);
 }
@@ -136,7 +133,7 @@ function createClickableElement(element, className, innerText, clickListener){
  * na sua respectiva li e atualizando o preço total.
  * @param {int} sku 
  */
- function addToCartItems(sku){
+ function addToCartItems(sku) {
   const olCartItems = document.getElementById('cart_items');
   
   getProductItem(sku)
@@ -147,7 +144,11 @@ function createClickableElement(element, className, innerText, clickListener){
   });
 }
 
-function addToCartClickListener(event){
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function addToCartClickListener(event) {
   const sku = getSkuFromProductItem(event.target.parentNode);
   addToCartItems(sku);
   addToLocalStorage(sku);
@@ -160,24 +161,20 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createClickableElement('button', 'item__add', 'Adicionar ao carrinho!', addToCartClickListener));
+  section.appendChild(createClickableElement('button', 'item__add', 
+    'Adicionar ao carrinho!', addToCartClickListener));
 
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
 }
 
 /**
  * função responsável por limpar o carrinho de compras.
  * Para isso, os elementos são removidos, o local storage
  * é limpo e o texto do preço do carrinho, zerado.
- * @param {PointerEvent} event 
  */
-function clearCartListener(event){
+function clearCartListener() {
   const cartItemsElement = document.getElementById('cart_items');
-  while(cartItemsElement.childNodes.length > 0){
+  while (cartItemsElement.childNodes.length > 0) {
     cartItemsElement.removeChild(cartItemsElement.lastChild);
   }
   
@@ -190,7 +187,7 @@ function clearCartListener(event){
 /**
  * função responsável por adicionar os produtos na seção items
  */
- function loadProducts(){
+ function loadProducts() {
   const itemsSection = document.getElementById('items');
   
   // adicionando a div de carregamento antes da requisição à API
@@ -198,18 +195,17 @@ function clearCartListener(event){
   itemsSection.appendChild(loadingDiv);
 
   getProducts()
-  .then( products => products.map(createProductItemElement))
-  .then( productItemElements => productItemElements
-    .forEach(productItemElement => itemsSection.appendChild(productItemElement)) 
-  )
+  .then((products) => products.map(createProductItemElement))
+  .then((productItemElements) => productItemElements
+    .forEach((productItemElement) => itemsSection.appendChild(productItemElement)))
   .then(() => itemsSection.removeChild(loadingDiv)); // removendo a div de carregamento pós requisição.
 }
 
 /**
  * função responsável por carregar os produtos do carrinho do repo local (local storage)
  */
- function loadFromLocalStorage(){
-  if(localStorage.skus){
+ function loadFromLocalStorage() {
+  if (localStorage.skus) {
     const skus = JSON.parse(localStorage.skus);
     skus.forEach(addToCartItems);
   }
